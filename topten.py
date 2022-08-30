03c9933f849fbebd2cd2fce7e7e970ebf40600eb
Chris Greene <archgoon@gmail.com>
2022-08-29 22:26:56 -0700

    Update topten.py

--- topten.py
+++ topten.py
@@ -1,30 +1,31 @@
 import argparse
-import bs4
 import requests
 import os
 import colorama
+import json
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--count", type=int, default=20)
     options = parser.parse_args()
-    resp = requests.get("https://dojo.pwn.college/scoreboard")
-    soup = bs4.BeautifulSoup(resp.content, "lxml")
-
-    results = []
-    for row in soup.find_all("a"):
-        if "/users" in row.get("href"):
-            row = row.parent.parent.find_all("td")
-            emblem = os.path.basename(row[0].img["src"])
-            name = row[1].text.strip()
-            score = row[2].text.strip()
-            results.append((score, name, emblem))
-    logos = {"hacker.png": "💻", "fork.png": "♆"}
-    red = colorama.Fore.RED
-    yellow = colorama.Fore.YELLOW
-    reset = colorama.Fore.WHITE
-    for i, (score, name, emblem) in enumerate(results[:options.count], 1):
-        print(f"{i}. {yellow + name + reset} {score} {red+logos.get(emblem, 'OTHER')+reset}")
+    count = 0
+    i = 1
+    while count < options.count:
+        resp = requests.get(f"https://dojo.pwn.college/pwncollege_api/v1/scoreboard/cse466/dojo/{i}")
+        scores = json.loads(resp.content)["page_standings"]
+        cur_count = count
+        count += len(scores)
+        if count > options.count:
+            diff = count - options.count
+            scores = scores[:-diff]
+        red = colorama.Fore.RED
+        yellow = colorama.Fore.YELLOW
+        reset = colorama.Fore.WHITE
+        logos = {"hacker.png": "💻", "fork.png": "♆"}
+        for score in scores:
+            logo = "♆" if "fork.png" in score["symbol"] else "💻" 
+            print(f"{i}. {yellow + score['name'] + reset} {score['score']} {red}{logo}{reset}")
+            i += 1
 
 if __name__ == "__main__":
     main()
